12f0e29a7b787c0a4c21821486f1f70d80ba6413
Stefan Doerr <stefan.doerr@upf.edu>
2019-10-22 17:53:08 +0300

    add missing channel

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Build for conda from PyPI
-source deactivate test
+conda deactivate test
 
 export TAG_DESCRIBE=$(git describe)
 export BUILD_VERSION=$(echo $TAG_DESCRIBE | sed 's/-/ /g'  | awk '{print $1}')
 export BUILD_NUMBER=0
 
-conda build --python $CONDA_PY package/jobqueues/
+conda build --python $CONDA_PY package/jobqueues/ -c acellera
 
 bash -x ./ci/travis/conda_upload.sh;
