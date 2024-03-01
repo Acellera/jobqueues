ab19bb41390c98a7ceebb187a2bb12a5fa26b30f
Stefan Doerr <stefdoerr@gmail.com>
2024-03-01 16:35:01 +0200

    better versioning

--- jobqueues/__init__.py
+++ jobqueues/__init__.py
@@ -1,12 +1,13 @@
 from jobqueues.home import home as __home
 import os
 import logging.config
+from jobqueues import _version
 
-__version__ = "unpackaged"
+__version__ = _version.get_versions()["version"]
 
 try:
     logging.config.fileConfig(
         os.path.join(__home(), "logging.ini"), disable_existing_loggers=False
     )
-except:
+except Exception:
     print("JobQueues: Logging setup failed")
